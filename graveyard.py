52889a89b5b9afd82833a729924b050b37322852
¨martin-he543¨ <martin.he543@outlook.com>
2023-03-15 15:02:11 +0000

    Current Updated Version

---
+++ graveyard.py
@@ -0,0 +1,18 @@
+
+# np.loadtxt("FiscalFuries_21347/match96_score_board.csv", delimiter="\t", unpack=True, skiprows=1)
+# np.loadtxt("FiscalFuries_21347/match176_score_board.csv", delimiter="\t", unpack=True, skiprows=1)
+# Time96,Competitor96,Operation96,OrderId96,Instrument96,Side96,Volume96,Price96,Lifespan96,Fee96 = np.loadtxt("FiscalFuries_21347/match96_events.csv", delimiter="\t", unpack=True, skiprows=1)
+# Time176,Competitor176,Operation176,OrderId176,Instrument176,Side176,Volume176,Price176,Lifespan176,Fee176 = np.loadtxt("FiscalFuries_21347/match176_events.csv", delimiter="\t", unpack=True, skiprows=1)
+
+# team_names = np.linspace(0,6,7,dtype=int)
+# temp_file = open('FiscalFuries_21347/match96_score_board.csv', 'r')
+# Lines = temp_file.readlines()
+
+# for name in team_names:
+#     count = 0
+#     for line in Lines:
+#         count += 1
+#         if name in line:
+#             line.strip().replace("Tick","")
+            
+# array0, array1, array2, array3, array4, array5, array6, array7 = [],[],[],[],[],[],[]
